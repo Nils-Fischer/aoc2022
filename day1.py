97726925c726c0170b5d4c1391a88745a7420b53
Nils Fischer <nils-fischer7@web.de>
2022-12-06 22:03:02 +0100

    part two

--- day1.py
+++ day1.py
@@ -1,5 +1,13 @@
 with open("day1.txt", "r") as file:
     input = file.read()
 
-input = max([sum([int(n) for n in x.split("\n")]) for x in input.strip().split("\n\n")])
-print(input)
+# part one
+most = max([sum([int(n) for n in x.split("\n")]) for x in input.strip().split("\n\n")])
+print(most)
+
+# part two
+
+top_three = [sum([int(n) for n in x.split("\n")]) for x in input.strip().split("\n\n")]
+top_three.sort(reverse=True)
+top_three = top_three[0] + top_three[1] + top_three[2]
+print(top_three)
